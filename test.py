cc4cb9b6c85fd7cf65a745958cea8ac40f11b38e
Cyrille Cervantes <cyrille.cervantes.off@gmail.com>
2022-05-02 18:43:08 +0800

    minor correction

--- test.py
+++ test.py
@@ -26,7 +26,7 @@ def get_args_parser(add_help=True):
     # parser.add_argument("--data-path",                  default="drinks",     type=str, help="dataset path" )
     # parser.add_argument("--dataset",                    default="drinks",                 type=str, help="dataset name")
 
-    parser.add_argument("--model",                      default="fasterrcnn_mobilenet_v3_large_fpn ", type=str, help="model name")
+    parser.add_argument("--model",                      default="fasterrcnn_mobilenet_v3_large_fpn", type=str, help="model name")
     parser.add_argument("--device",                     default="cuda",                 type=str, help="device (Use cuda or cpu Default: cuda)")
     parser.add_argument("--data-augmentation",          default="hflip",                type=str, help="data augmentation policy (default: hflip)" )
     parser.add_argument("-j", "--workers",              default=4,                      type=int, metavar="N", help="number of data loading workers (default: 4)")
